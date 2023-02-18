9c4018e6c7649b091efbf98c9f7b5082326aa964
Sandipan Pradhan <112494605+MoharajOP7z@users.noreply.github.com>
2023-02-18 21:01:17 +0530

    update 0.0.2
    data 18.03.23 update java ass1
    
    Co-Authored-By: Sayan209191 <121744189+Sayan209191@users.noreply.github.com>

---
+++ object-oriented-programming-lab/assignment_1/j_p_ass2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class j_p_ass2 {
+
+    public static void main(String[] args) {
+
+        int x,y,sum;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input a number : ");
+        x=sc.nextInt();
+        System.out.print("Input another number : ");
+        y=sc.nextInt();
+        sum=x+y;
+        System.out.println("the total :" + sum);
+}
+}
---
+++ object-oriented-programming-lab/assignment_1/j_p_ass3.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class j_p_ass3 {
+
+    public static void main(String[] args) {
+        float Fahrenheit,Celsius;
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Input Fahrenheit : ");
+        Fahrenheit = sc.nextFloat();
+        Celsius = (float) ((Fahrenheit - 32)*(0.5556));
+        System.out.println("Input Celsius : " + Celsius); 
+}}
